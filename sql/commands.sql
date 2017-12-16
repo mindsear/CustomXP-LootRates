da1af2b9d48a09f85a30f919e3d4ca1f61e8cac2
mindsear <xxusername12843@yahoo.ro>
2017-12-16 20:12:00 +0200

    Add files via upload

---
+++ sql/commands.sql
@@ -0,0 +1,4 @@
+insert  into `world`.`command`(`name`,`permission`,`help`) values 
+('rate xp',1008,'Syntax: .rate xp $value\r\nSets your XP multiplier to $value. If no parameter is provided, it will show your current XP rate.'),
+('rate loot',1009,'Syntax: .rate loot $value\r\nSets your loot multiplier to $value. If no parameter is provided, it will show your current loot rate. A $value of 0 means you won\'t be able to loot anything.'),
+('rate',1010,'Syntax: .rate xp $value\r\nSets your XP multiplier to $value. If no parameter is provided, it will show your current XP rate, same with loot.');
